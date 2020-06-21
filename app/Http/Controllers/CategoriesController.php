7fd66592ac9e9f823708240701f04ccef38e38f4
Manisha Malhotra <63730859+manishamalhotra135@users.noreply.github.com>
2020-06-21 20:39:34 +0530

    Add files via upload
    app folder

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categories;
+use Illuminate\Http\Request;
+use App\Http\Requests\Categories\CreateCategoryRequest;
+use App\Http\Requests\Categories\UpdateCategoryRequest;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('categories.index')->with('categories',Categories::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      return view('Categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCategoryRequest $request)
+    {
+        
+       
+        Categories::create([
+            'name'=>$request->name
+        ]);
+
+        session()->flash('success','category created successfully');
+        return redirect(route('categories.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categories $categories)
+    {
+        return view('categories.create')->with('categories',$categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, Categories $categories)
+    {
+      $categories->name = $request->name;
+      $categories->save();
+      session()->flash('success','Category Updated Successfully');
+      return redirect(route('categories.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categories $categories)
+    { 
+        $categories->delete();
+        session()->flash('success','Category Deleted Successfully');
+        return redirect(route('categories.index'));
+    }
+}
